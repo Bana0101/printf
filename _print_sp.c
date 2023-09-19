f48b94ac7d47fccbe12782274e3311fe9acd9ccd
n1o2h <aythmadnhylt@gmail.com>
2023-09-19 16:37:05 -0700

    done

---
+++ _print_sp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  _print_sp - function check ifthe format is
+ *  d or i or c or s or %
+ * @format: format
+ * Return: count
+ */
+
+int _print_sp(const char *format, va_list args)
+{
+	int count = 0;
+
+	if (*format == 'c')
+		count += _putchar(va_arg(args, int));
+	else if (*format == 's')
+		count += _print_str(va_arg(args, char*));
+	else if (*format == '%')
+		count += _putchar('%');
+	else if (*format == 'd')
+		count += _print_num(va_arg(args, int));
+	else if (*format == 'i')
+		count += _print_num(va_arg(args, int));
+	return (count);
+}
