5ec6a54539540a5df4c6566928fa803313f90a84
n1o2h <aythmadnhylt@gmail.com>
2023-09-19 17:42:00 -0700

    done

--- main.h
+++ main.h
@@ -10,5 +10,6 @@ int _print_str(char *str);
 void _print_int(int n);
 int _print_num(int n);
 int _print_sp(const char *format, va_list args);
+int _peint_un(unsigned int);
 int _binary(int n);
 #endif
